128263b90179dab9daea579892dcb5e06048bac0
tu-cis <52547702+tu-cis@users.noreply.github.com>
2021-10-25 12:49:16 -0500

    Add files via upload

---
+++ fizzBuzz.js
@@ -0,0 +1,29 @@
+var $ = function (id) 
+{
+    return document.getElementById(id);
+}
+
+doStuffComputer = function()
+{
+	var counter = 0;
+	var output = "";
+	
+//insert code below    
+    
+    
+    
+    
+    
+    
+    
+    
+	alert(output);
+	
+}
+
+
+
+window.onload = function()
+{
+	$("doIt").onclick = doStuffComputer;
+}

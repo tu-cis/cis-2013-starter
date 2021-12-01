e3df76087c518a14fb0c13f4306eade3cd6ad8d0
tu-cis <52547702+tu-cis@users.noreply.github.com>
2021-12-01 12:16:27 -0600

    Add files via upload

--- phrase2.js
+++ phrase2.js
@@ -4,63 +4,18 @@ var $ = function (id)
 }
 
 var vowels = function () 
-{
-	var stringEntry = $("phrase").value;
-	stringEntry = stringEntry.toUpperCase();
-	if (stringEntry==="")
-	{
-		$("phrase").value = "";
-		alert("Please enter a phrase and THEN select a button...");
-	}
-	else
-	{
-		var intVowels = 0;
-		//insert for loop here
-		
-		
-		$("output").value = "There are " + intVowels + " vowels in the above phrase.";
-	}	
-} 
+
+
 
 var consonants = function () 
-{
-	var stringEntry = $("phrase").value;
-	stringEntry = stringEntry.toLowerCase();
-	
-	if (stringEntry==="")
-	{
-		$("phrase").value = "";
-		alert("Please enter a phrase and THEN select a button...");
-	}
-	else
-	{
-		var intConsonants = 0;
-		// insert for loop here
-		
-		
-		
-		$("output").value = "There are " + intConsonants + " consonants in the above phrase.";
-	}
-}
+
+
+
 
 var reverse = function () 
-{
-	var stringEntry = $("phrase").value;
-	if (stringEntry==="")
-	{
-		$("phrase").value = "";
-		alert("Please enter a phrase and THEN select a button...");
-	}
-	else
-	{
-	
-		var stringReversed = "";
-		// insert for loop here
-		
-		
-		$("output").value = "The reverse of the above phrase is:\n" + stringReversed;
-	}
-}
+
+
+
 
 var clear = function () 
 {

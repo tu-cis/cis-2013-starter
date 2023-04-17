35051f54e159fced0525baf2970a96a1b7268f8b
tu-cis <52547702+tu-cis@users.noreply.github.com>
2023-04-17 12:27:40 -0500

    Update ArrayPracticeEC.js

--- ArrayPracticeEC.js
+++ ArrayPracticeEC.js
@@ -1,4 +1,17 @@
 //Task 1
+var arrayNames = new Array('John', 'Robert', 'Jimmy', 'Bonzo');
+var arrayInstruments = [];
+arrayInstruments[0] = "bass";
+arrayInstruments[1] = "vocals";
+arrayInstruments[2] = "guitar";
+arrayInstruments[3] = "drums";
+var stringMessage = "";
+var intCount;
+for (intCount = 0; intCount < arrayNames.length; intCount++)
+{
+stringMessage = stringMessage + arrayNames[intCount] + " plays " + arrayInstruments[intCount] +"\n";
+}
+alert(stringMessage);
 
 
 
